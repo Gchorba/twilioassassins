1838006b7c15e0aa4c36bd3fd490c7272d6e8bdc
Matt Schulte <schultetwin1@gmail.com>
2013-01-19 00:27:31 -0500

    Added join game command

--- run.py
+++ run.py
@@ -8,12 +8,33 @@ games = {}
 
 @app.route("/", methods=['GET', 'POST'])
 def register():
+  resp_str = ""
   number = request.values.get('From', None)
-  name = request.values.get('Body', None)
-  players[number] = name;
+  # Remove '+' from start
+  number = number[1:]
+  body = request.values.get('Body', None)
+  resp_str = parse_msg(number, body)
+  if resp_str == "":
+    print "Not sure how to parse " + body
+  else:
+    print resp_str
   resp = twilio.twiml.Response()
-  resp.sms("Hello, " + name)
+  resp.sms(resp_str)
   return str(resp)
+
+def parse_msg(number, msg):
+  # check for join
+  if len(msg) > 5 and msg[0:5].lower() == "join ":
+    game = msg[5:].strip()
+    if join_game(number, game):
+      return "Successfully joined " + game
+    else:
+      return "Did not join " + game
+  return ""
+
+def join_game(number, game_name):
+  # check to make sure number isnt in game
+  return True
 
 if __name__ == "__main__":
   app.debug = True
