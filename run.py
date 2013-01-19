c44033826fbca2a9e66b5bafd867cca1ce82f9ec
Matt Schulte <schultetwin1@gmail.com>
2013-01-19 00:56:49 -0500

    Added a lot more functionality. You can now kill yourself

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 from flask import Flask, request, redirect
 import twilio.twiml
+from twilio.rest import TwilioRestClient
+
 import config
 import sqlite3
 import os
@@ -9,6 +11,10 @@ app = Flask(__name__)
 
 conn = sqlite3.connect(config.db_filename)
 cursor = conn.cursor()
+
+account_sid = "AC75b70fcc8bd7e0a0f07b3ba0aa565de4"
+auth_token = "852c4fccd882910e1b0bc0bb7496b873"
+client = TwilioRestClient(account_sid, auth_token)
 
 players = {}
 games = {}
@@ -29,6 +35,7 @@ def register():
   resp.sms(resp_str)
   return str(resp)
 
+# returns error message
 def parse_msg(number, msg):
   # check for join
   if len(msg) > 5 and msg[0:5].lower() == "join ":
@@ -37,7 +44,51 @@ def parse_msg(number, msg):
       return "Successfully joined " + game
     else:
       return "Did not join " + game
+
+  # check for death
+  if msg.strip().lower() == "death":
+    # Send message to killer
+    killer = get_killer(number)
+    new_assigned = get_assigned(number)
+    remove_player(number)
+    assign(killer, new_assigned)
+    return "Your death has been updated. Thanks for playing"
+
   return ""
+
+# Should add name to number
+# Throw exception if name does not exist in any game
+# Throw exception if already have a name
+# @TODO: Mark
+def add_name(number, name):
+  return
+
+# Gets the name based on the phone number
+# @TODO: Mark
+def get_name(number):
+  return "name"
+
+# Gets the number of the person who was supposed to kill the dead_number
+# @TODO: Mark
+def get_killer(dead):
+  return 0
+
+# Returns the person this guy is assigned
+# @TODO: Mark
+def get_assigned(number):
+  return 0
+
+# Removes player from game
+def remove_player(number):
+  return 0
+  
+def assign(killer, kille):
+  # Should do something in the DB as well
+  message = client.sms.messages.create(
+              to="+" + str(killer), 
+              from_="+17342746365", 
+              body= "New Assignment: " + get_name(kille)
+            )
 
 def join_game(number, game_name):
   # check to make sure number isnt in game
